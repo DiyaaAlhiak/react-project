e24c72a59593a83d501da0b2f2bc58ebe73478f4
Diyaa Alhaik <diyaahaik77@gmail.com>
2023-05-09 15:52:06 +0300

    react-project

--- src/components/Connectors/CompConnectors.js
+++ src/components/Connectors/CompConnectors.js
@@ -1,9 +1,57 @@
 import React from 'react'
 import './CompConnectors.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import  Input  from '../../shared/components/form/Inpot/Input';
+import Form from 'react-bootstrap/Form';
+import BtnSearch from '../../shared/components/buttons/search/BtnSearch'
 function CompConnectors() {
   return (
-    <div className='img'>
-    
+    <div >
+   <div className='img'>
+<div className='m-5'>
+  <h2 className='text-body-emphasis'>Where do you want to go</h2>
+  <p className='text-dark fs-4'>Jahizen at any time</p>
+</div>
+   </div>
+  
+   <Container>
+      <Row xs={1} md={2} lg={2}>
+        <Col>
+        <Form.Label>Email address</Form.Label>
+        <Input type="text"/>
+        </Col>
+        <Col>
+        <Form.Label>Email address</Form.Label>
+        <Input type="text"/>
+        </Col>
+      </Row>
+      <Row xs={1} md={2} lg={2}>
+        <Col>
+        <Form.Label>Email address</Form.Label>
+        <Input type="date"/>
+        </Col>
+        <Col>
+        <Form.Label>Email address</Form.Label>
+        <Input type="date"/>
+        </Col>
+      </Row>
+      <Row xs={1} md={2} lg={2}>
+        <Col>
+        <Form.Label>Email address</Form.Label>
+        <Input type="number"/>
+        </Col>
+        <Col>
+        <Form.Label>Email address</Form.Label>
+        <Input type="number"/>
+        </Col>
+      </Row>
+    </Container>
+<div >
+<BtnSearch />
+</div>
+  
     </div>
   )
 }
--- src/shared/components/buttons/search/BtnSearch.js
+++ src/shared/components/buttons/search/BtnSearch.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './BtnSearch.css'
+import Button from 'react-bootstrap/Button';
  function BtnSearch() {
   return (
     <div>
-      
+    <Button variant="primary">Primary</Button>
     </div>
   )
 }
+
 export default BtnSearch;
