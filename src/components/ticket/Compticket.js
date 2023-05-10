799cdb9e3b5463cbe5f5da850199cdd96760c06a
Diyaa Alhaik <diyaahaik77@gmail.com>
2023-05-10 10:44:54 +0300

    'react'

--- src/components/ticket/Compticket.js
+++ src/components/ticket/Compticket.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Compticket.css'
+import Card from 'react-bootstrap/Card';
  function Compticket() {
   return (
-    <div >
-Compticket
+    <div className='mt-4 '>
+    <h2 className='text-primary mb-4'>Select Ticket</h2>
+    <Card className="text-center">
+      <Card.Header ></Card.Header>
+      <Card.Body>
+      <Card.Text>Departure City</Card.Text>
+        <Card.Title>Jakarta Station</Card.Title>
+        <h2 className='text-primary'>To</h2>
+        <Card.Text>City of Arrival</Card.Text>
+        <Card.Title>Sukabumi Station</Card.Title>
+      
+      </Card.Body>
+      <Card.Footer className="text-muted"></Card.Footer>
+    </Card>
     </div>
   )
 }
